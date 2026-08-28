da38cb04f826eb23d6e654f5fd8f35ac7a6ce49e
MuhammadAsif46 <asifahmed32c@gmail.com>
2026-08-28 11:18:46 +0500

    routing add

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,26 @@ import Card from './components/Card.jsx'
 import Header from './components/Header.jsx'
 import UseFetch from './hooks/UseFetch.js'
 import Footer from './components/Footer.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
 
 
   return (
-    <>
-      <Header />
+    <BrowserRouter>
+      {/* <Header />
       <h1 className='text-3xl font-bold underline'>Hello</h1>
       <p className='text-green-500'>hello world</p>
       <Card />
-      <Footer />
-    </>
+      <Footer /> */}
+
+      <Routes>
+        <Route path='/' element={<Header />} />
+        <Route path='/about' element={<Card />} />
+        <Route path='/footer' element={<Footer />} />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
