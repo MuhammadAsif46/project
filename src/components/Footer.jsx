e83294404d5f235452683aa57b172ad42824c985
MuhammadAsif46 <asifahmed32c@gmail.com>
2026-08-28 11:41:14 +0500

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,42 +1,42 @@
-import React from 'react'
+// import React from 'react'
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
-
-export default Footer
-
-
-// import { Component } from "react";
-
-
-// class Footer extends Component {
-
-//     state = {
-//         count: 0
-//     }
-
-//     componentDidMount()
-//     componentDidUpdate()
-//     componentWillUnmount()
-
-//     render() {
-//         return (
-//             <div>
-//                 <h1>Footer Component</h1>
-//                 <h2>Counter : {this.state.count}</h2>
-//                 {/* <button onClick={() => this.setState({
-//                     count: count + 1
-//                 })}>Increment</button> */}
-//                 <button onClick={() => this.setState((prevState) => ({
-//                     count: prevState.count + 1
-//                 }))}>Increment</button>
-//                 {/* <button>Decrement</button> */}
-//             </div >
-//         )
-//     }
+// const Footer = () => {
+//   return (
+//     <div>Footer</div>
+//   )
 // }
 
 // export default Footer
+
+
+import { Component } from "react";
+
+
+class Footer extends Component {
+
+    state = {
+        count: 0
+    }
+
+    // componentDidMount()
+    // componentDidUpdate()
+    // componentWillUnmount()
+
+    render() {
+        return (
+            <div>
+                <h1>Footer Component</h1>
+                <h2>Counter : {this.state.count}</h2>
+                {/* <button onClick={() => this.setState({
+                    count: count + 1
+                })}>Increment</button> */}
+                <button onClick={() => this.setState((prevState) => ({
+                    count: prevState.count + 1
+                }))}>Increment</button>
+                {/* <button>Decrement</button> */}
+            </div >
+        )
+    }
+}
+
+export default Footer
